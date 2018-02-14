872aa800cc24f3542bb00c0671f88bd9046b2b76
flavio <flaviolzsantos@gmail.com>
2018-02-14 19:40:25 -0200

    add helmet

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,16 +1,17 @@
 let express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    helmet = require('helmet');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(bodyParser.json());
+app.use(helmet());
 
 app.use(function (req, res, next) {
-    
+    console.log(req.path);
     res.setHeader('Access-Control-Allow-Origin', '*');        
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
